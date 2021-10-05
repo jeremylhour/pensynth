fix numercail overflow

incremental_algo_puresynth/main.py
@@ -67,16 +67,14 @@ if __name__=='__main__':
         print(f"    {i+1} out of {len(X1_full)}.")
         sameAsUntreated = np.all(X0==x, axis=1) # True if untreated is same as treated
         if any(sameAsUntreated):
+            print("SAME AS UNTREATED")
             untreatedId = np.where(sameAsUntreated)
             allW[i, untreatedId] = 1/len(untreatedId)
         else:
             inHullFlag = in_hull(x=x, points=X0)
             if inHullFlag:
-                X0_tilde, antiranks, numericalError = incremental_pure_synth(X1=x, X0=X0)
-                if numericalError: # if numerical error when computing the reduced simplex
-                    allW[i, antiranks] = pensynth_weights(X0=X0_tilde, X1=x, pen=5e-4)
-                else:
-                    allW[i, antiranks] = pensynth_weights(X0=X0_tilde, X1=x, pen=0)
+                X0_tilde, antiranks, _ = incremental_pure_synth(X1=x, X0=X0)
+                allW[i, antiranks] = pensynth_weights(X0=X0_tilde, X1=x, pen=0)
             else:
                 allW[i,] = pensynth_weights(X0=X0, X1=x, pen=1e-6)
     print(f"Time elapsed : {(time.time() - start_time):.2f} seconds ---")
@@ -113,3 +111,4 @@ if __name__=='__main__':
     ########## SANITY CHECK ON SPARSITY ##########
     high_sparsity = np.where(sparsity_index>11)[0]
     print(f'{len(high_sparsity)} treated units have sparsity larger than p+1.')
+    print(high_sparsity)

incremental_algo_puresynth/pensynthpy.py
@@ -78,6 +78,8 @@ def compute_radius_and_barycenter(nodes):
     
     Source:
         https://math.stackexchange.com/questions/1087011/calculating-the-radius-of-the-circumscribed-sphere-of-an-arbitrary-tetrahedron
+        
+    Note : normally, it should return np.sqrt(-a[0]/2), a[1:] @ nodes, but overflow can occur so I force a positive value
     """
     p = nodes.shape[1]
     
@@ -87,7 +89,7 @@ def compute_radius_and_barycenter(nodes):
     Delta[1:,1:] = cdist(nodes, nodes)**2
     
     a = np.linalg.inv(Delta)[:,0]
-    return np.sqrt(-a[0]/2), np.matmul(a[1:],nodes)
+    return np.sqrt(np.abs(a[0])/2), a[1:] @ nodes
       
 @njit
 def inside_sphere(nodes, barycenter, radius):
@@ -166,7 +168,7 @@ def incremental_pure_synth(X1, X0):
                 
                 if np.isnan(radius): # if there is a degenerate case, we stop
                     numericalError = True
-                    #the_simplex = tuple([i for i in range(k-1)])
+                    the_simplex = candidate
                     foundIt = True
                     break
             