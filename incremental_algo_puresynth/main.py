ef2bb77fbb0be845b58b94af51c3976d935616c2
Jérémy L'Hour <11871612+jeremylhour@users.noreply.github.com>
2025-03-01 17:52:43 +0100

    python code cleanup

--- incremental_algo_puresynth/main.py
+++ incremental_algo_puresynth/main.py
@@ -7,116 +7,131 @@ Created on Sun Nov  1 10:51:38 2020
 
 @author: jeremylhour
 """
+
 import numpy as np
 import pandas as pd
 import time
 from datetime import datetime
 from tqdm import tqdm
 
-from pensynthpy import in_hull, incremental_pure_synth, pensynth_weights
+from pensynthpy import is_in_hull, incremental_pure_synth, pensynth_weights
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     now = datetime.now()
-    print('This is a script to compute the pure synthetic control solution for Lalonde (1986) data.\n')
+    print(
+        "This is a script to compute the pure synthetic control solution for Lalonde (1986) data.\n"
+    )
     print(f"Launched on {now.strftime('%d, %b %Y, %H:%M:%S')} \n")
     print("Note : run downloadLalondeData.R script first.\n")
-    
-    
-    print("="*80)
+
+    print("=" * 80)
     print("DATA MANAGEMENT")
-    print("="*80)
-    
-    DATA_PATH = '../data/'
-    
+    print("=" * 80)
+
+    DATA_PATH = "../data/"
+
     ### Loading Lalonde's dataset rescaled as in the paper and unscaled for statistics
-    X1_full = np.loadtxt(DATA_PATH+'X1.txt', skiprows=1)
-    Y1_full = np.loadtxt(DATA_PATH+'Y1.txt', skiprows=1)
-    X0_full = np.loadtxt(DATA_PATH+'X0.txt', skiprows=1)
-    Y0_full = np.loadtxt(DATA_PATH+'Y0.txt', skiprows=1)
-    X0_unscaled_full = np.loadtxt(DATA_PATH+'X0_unscaled.txt', skiprows=1)
+    X1_full = np.loadtxt(DATA_PATH + "X1.txt", skiprows=1)
+    Y1_full = np.loadtxt(DATA_PATH + "Y1.txt", skiprows=1)
+    X0_full = np.loadtxt(DATA_PATH + "X0.txt", skiprows=1)
+    Y0_full = np.loadtxt(DATA_PATH + "Y0.txt", skiprows=1)
+    X0_unscaled_full = np.loadtxt(DATA_PATH + "X0_unscaled.txt", skiprows=1)
 
     ### Consolidate duplicates in X0
-    X_names = ['age', 'education', 'married', 'black', 'hispanic', 're74', 're75', 'nodegree', 'NoIncome74', 'NoIncome75']
+    X_names = [
+        "age",
+        "education",
+        "married",
+        "black",
+        "hispanic",
+        "re74",
+        "re75",
+        "nodegree",
+        "NoIncome74",
+        "NoIncome75",
+    ]
     df = pd.DataFrame(X0_full)
-    df.columns = [item+'_rescaled' for item in X_names]
-    df['outcome'] = Y0_full
-    
+    df.columns = [item + "_rescaled" for item in X_names]
+    df["outcome"] = Y0_full
+
     for i in range(X0_unscaled_full.shape[1]):
-        df[X_names[i]] = X0_unscaled_full[:,i]
+        df[X_names[i]] = X0_unscaled_full[:, i]
 
     ### Consolidate dataset for untreated
-    df_consolidated = df.groupby(X_names)[[i+'_rescaled' for i in X_names] + ['outcome'] + X_names].mean()
-    X0 = df_consolidated[[i+'_rescaled' for i in X_names]].to_numpy()
+    df_consolidated = df.groupby(X_names)[
+        [i + "_rescaled" for i in X_names] + ["outcome"] + X_names
+    ].mean()
+    X0 = df_consolidated[[i + "_rescaled" for i in X_names]].to_numpy()
     X0_unscaled = df_consolidated[X_names].to_numpy()
-    Y0 = df_consolidated['outcome'].to_numpy()
+    Y0 = df_consolidated["outcome"].to_numpy()
 
-
-    print("="*80)
+    print("=" * 80)
     print("COMPUTING PURE SYNTHETIC CONTROL FOR EACH TREATED")
-    print("="*80)
-    
+    print("=" * 80)
+
     # We proceed in 3 steps :
     # - if some untreated are the same as the treated, assign uniform weights to these untreated.
     # - if the treated is inside the convex hull defined by the untreated, run the incremental algo.
     # - if the treated is not inside the convex hull defined by the untreated, run the standard synthetic control.
-    
+
     allW = np.zeros((len(X1_full), len(X0)))
     start_time = time.time()
     with tqdm(total=(len(X1_full))) as prog:
         for i, x in enumerate(X1_full):
-            sameAsUntreated = np.all(X0==x, axis=1) # True if untreated is same as treated
+            sameAsUntreated = np.all(
+                X0 == x, axis=1
+            )  # True if untreated is same as treated
             if any(sameAsUntreated):
                 untreatedId = np.where(sameAsUntreated)
-                allW[i, untreatedId] = 1/len(untreatedId)
+                allW[i, untreatedId] = 1 / len(untreatedId)
             else:
-                inHullFlag = in_hull(x=x, points=X0)
+                inHullFlag = is_in_hull(x=x, points=X0)
                 if inHullFlag:
                     X0_tilde, antiranks = incremental_pure_synth(X1=x, X0=X0)
                     allW[i, antiranks] = pensynth_weights(X0=X0_tilde, X1=x, pen=0)
                 else:
                     allW[i,] = pensynth_weights(X0=X0, X1=x, pen=1e-6)
             prog.update(1)
-    print(f"Time elapsed : {(time.time() - start_time):.2f} seconds ---")
+    print(f"Time elapsed : {(time.time() - start_time):.2f} sec")
 
-
-    print("="*80)
+    print("=" * 80)
     print("COMPUTING STATISTICS AND SAVING RESULTS")
-    print("="*80)
+    print("=" * 80)
 
     ########## COMPUTE THE NECESSARY STATISTICS ##########
     Y0_hat = allW @ Y0
     balance_check = (allW @ X0_unscaled).mean(axis=0)
 
-    print('ATT: {:.3f}'.format((Y1_full - Y0_hat).mean(axis=0)))
+    print("ATT: {:.3f}".format((Y1_full - Y0_hat).mean(axis=0)))
 
     for b, value in enumerate(balance_check):
-        print(X_names[b] +': {:.3f}'.format(value))
+        print(X_names[b] + ": {:.3f}".format(value))
 
     sparsity_index = (allW > 0).sum(axis=1)
-    print('Min sparsity: {:.0f}'.format(sparsity_index.min()))
-    print('Median sparsity: {:.0f}'.format(np.median(sparsity_index)))
-    print('Max sparsity: {:.0f}'.format(sparsity_index.max()))
+    print("Min sparsity: {:.0f}".format(sparsity_index.min()))
+    print("Median sparsity: {:.0f}".format(np.median(sparsity_index)))
+    print("Max sparsity: {:.0f}".format(sparsity_index.max()))
 
-    activ_index = (allW > 0).sum(axis=0)>0
-    print('Active untreated units: {:.0f}'.format(activ_index.sum()))
-    
+    activ_index = (allW > 0).sum(axis=0) > 0
+    print("Active untreated units: {:.0f}".format(activ_index.sum()))
+
     ########## SAVING WEIGHTS AS PARQUET FILE ##########
     df = pd.DataFrame(allW)
-    df.columns = ["Unit_"+str(i+1) for i in range(len(X0))]
+    df.columns = ["Unit_" + str(i + 1) for i in range(len(X0))]
     df.to_parquet("Lalonde_solution.parquet", engine="pyarrow")
-    
+
     ########## SANITY CHECK ON SPARSITY ##########
-    high_sparsity = np.where(sparsity_index>11)[0]
-    print(f'{len(high_sparsity)} treated units have sparsity larger than p+1.')
+    high_sparsity = np.where(sparsity_index > 11)[0]
+    print(f"{len(high_sparsity)} treated units have sparsity larger than p+1.")
     print(high_sparsity)
-    
+
     ########## DUMPING STATS TO FILE ##########
-    with open('statistics.txt', 'w') as f:
-        f.write('ATT: {:.3f}\n'.format((Y1_full - Y0_hat).mean(axis=0)))
+    with open("statistics.txt", "w") as f:
+        f.write("ATT: {:.3f}\n".format((Y1_full - Y0_hat).mean(axis=0)))
         for b, value in enumerate(balance_check):
-            f.write(X_names[b] +': {:.3f}\n'.format(value))
-        f.write('Min sparsity: {:.0f}\n'.format(sparsity_index.min()))
-        f.write('Median sparsity: {:.0f}\n'.format(np.median(sparsity_index)))
-        f.write('Max sparsity: {:.0f}\n'.format(sparsity_index.max()))
-        f.write(f'{len(high_sparsity)} treated units have sparsity larger than p+1.')
+            f.write(X_names[b] + ": {:.3f}\n".format(value))
+        f.write("Min sparsity: {:.0f}\n".format(sparsity_index.min()))
+        f.write("Median sparsity: {:.0f}\n".format(np.median(sparsity_index)))
+        f.write("Max sparsity: {:.0f}\n".format(sparsity_index.max()))
+        f.write(f"{len(high_sparsity)} treated units have sparsity larger than p+1.")
--- incremental_algo_puresynth/pensynthpy.py
+++ incremental_algo_puresynth/pensynthpy.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Functions used to run the in-house algorithm to
 incrementally compute the Delaunay triangulation
@@ -9,6 +7,7 @@ Refactored : 30/07/2021
 
 @author: jeremylhour
 """
+
 import numpy as np
 import itertools
 import time
@@ -21,103 +20,135 @@ import warnings
 
 from scipy.spatial import Delaunay
 
+solvers.options["show_progress"] = False
+solvers.options["abstol"] = 1e-8
+solvers.options["reltol"] = 1e-8
+solvers.options["maxiters"] = 500
+
+
 # ------------------------------------------------------------------------------
-# UTILS
+# UNIT FUNCTIONS
 # ------------------------------------------------------------------------------
 @njit
-def closest_points(node, nodes, k=1):
+def find_knn(node, nodes, k: int = 1):
     """
-    closest_points:
-        find the k nearest neighbors
-        
-    @param node (np.array): point for which we want to find the neighbors
-    @param nodes (np.array): points that are candidate neighbors
-    @param k (int): how many neighbors to return?
+    find_knn:
+        find the k nearest neighbors of node amongst nodes.
+
+    Args:
+        node (np.array): point for which we want to find the neighbors
+        nodes (np.array): points that are candidate neighbors
+        k (int): how many neighbors to return?
+
+    Returns:
+        np.array: the k nearest neighbors of node amongst nodes.
     """
-    dist_2 = np.diag((nodes - node) @ np.transpose(nodes - node))
+    dist_2 = np.diag((nodes - node) @ (nodes - node).T)
     ranks = [sorted(dist_2).index(x) for x in dist_2]
-    return nodes[[r<=k-1 for r in ranks]]
+    return nodes[[r <= k - 1 for r in ranks]]
+
 
 @njit
 def get_ranks(node, nodes):
     """
     get_ranks:
-        returns the ranks and anti-ranks of nodes by rank in closeness to node
-        
-    @param node (np.array): point for which we want to find the neighbors
-    @param nodes (np.array): points that are candidate neighbors
+        returns the ranks and anti-ranks of nodes by rank in closeness to node.
+
+    Args:
+        node (np.array): point for which we want to find the neighbors
+        nodes (np.array): points that are candidate neighbors
+
+    Returns:
+        np.array: the ranks of nodes by rank in closeness to node.
+        np.array: the anti-ranks of nodes by rank in closeness to node.
     """
-    dist_2 = np.diag((nodes - node) @ np.transpose(nodes - node))
+    dist_2 = np.diag((nodes - node) @ (nodes - node).T)
     ranks = np.array([sorted(dist_2).index(x) for x in dist_2])
     return ranks, np.argsort(ranks)
 
-def in_hull(x, points):
+
+def is_in_hull(x, points):
     """
-    in_hull:
+    is_in_hull:
         test if points in x are in hull
 
-    @param x (np.array):  should be a n x p coordinates of n points in p dimensions
-    @param points (np.array): the m x p array of the coordinates of m points in p dimensions 
+    Args:
+        x (np.array): should be a (n, p) coordinates of n points in p dimensions
+        points (np.array): the (m, p) array of the coordinates of m points in p dimensions
+
+    Returns:
+        bool: True if x is in the convex hull of points, False.
     """
     n_points = len(points)
     c = np.zeros(n_points)
-    A = np.r_[points.T,np.ones((1,n_points))]
+    A = np.r_[points.T, np.ones((1, n_points))]
     b = np.r_[x, np.ones(1)]
-    with warnings.catch_warnings(): # to ignore warning when degenerate cases
+    with warnings.catch_warnings():  # to ignore warning when degenerate cases
         warnings.simplefilter("ignore")
         lp = linprog(c, A_eq=A, b_eq=b)
     return lp.success
 
-def compute_radius_and_barycenter(nodes, fix_overflow=True):
+
+def compute_radius_and_barycenter(nodes, fix_overflow: bool = True):
     """
-    compute_radius_and_barycenter: 
+    compute_radius_and_barycenter:
         returns radius, coordinates of barycenter
         for circumscribed hypersphere for these points
-        
-    @param nodes (np.array): array of dimension (p+1) x p of the p+1 points in p dimension
-    @param fix_overflow (bool): if True brutally fixes the overflow
-    
+
+    Note: normally, it should return np.sqrt(-a[0]/2), a[1:] @ nodes, but overflow can occur so I force a positive value inside sqrt.
+
     Source:
         https://math.stackexchange.com/questions/1087011/calculating-the-radius-of-the-circumscribed-sphere-of-an-arbitrary-tetrahedron
-        
-    Note : normally, it should return np.sqrt(-a[0]/2), a[1:] @ nodes, but overflow can occur so I force a positive value inside sqrt
+
+    Args:
+        nodes (np.array): array of dimension (p+1) x p of the p+1 points in p dimension
+        fix_overflow (bool): if True brutally fixes the overflow
+
+    Returns:
+        float: radius of the circumscribed hypersphere
+        np.array: coordinates of the barycenter
     """
     p = nodes.shape[1]
-    
-    Delta = np.zeros((p+2, p+2))
-    Delta[0,] = np.concatenate(([0], np.ones(p+1)), axis=0)
-    Delta[:,0] = np.concatenate(([0], np.ones(p+1)), axis=0)
-    Delta[1:,1:] = cdist(nodes, nodes)**2
-    
-    a = np.linalg.inv(Delta)[:,0]
+
+    Delta = np.zeros((p + 2, p + 2))
+    Delta[0,] = np.concatenate(([0], np.ones(p + 1)), axis=0)
+    Delta[:, 0] = np.concatenate(([0], np.ones(p + 1)), axis=0)
+    Delta[1:, 1:] = cdist(nodes, nodes) ** 2
+
+    a = np.linalg.inv(Delta)[:, 0]
     if fix_overflow:
-        return np.sqrt(np.abs(a[0])/2), a[1:] @ nodes
+        return np.sqrt(np.abs(a[0]) / 2), a[1:] @ nodes
     else:
-        return np.sqrt(-a[0]/2), a[1:] @ nodes
-@njit
-def inside_sphere(nodes, barycenter, radius):
-    """
-    inside_ball: 
-        find if any of the nodes is inside the given sphere
-        
-    @param nodes (np.array): points to check if inside
-    @param barycenter (np.array): coordinates of the barycenter
-    @param radius (float): radius
-    """
-    dist_2 = np.diag((nodes - barycenter) @ np.transpose(nodes - barycenter))
-    return np.any(np.array([item < radius**2 for item in dist_2]))
+        return np.sqrt(-a[0] / 2), a[1:] @ nodes
+
 
 @njit
-def Tzero(w, tol=1e-5):
+def is_inside_sphere(nodes, barycenter, radius: float):
     """
-    Tzero:
+    inside_ball:
+        find if any of the nodes is inside the given sphere
+
+    Args:
+        nodes (np.array): points to check if inside
+        barycenter (np.array): coordinates of the barycenter
+        radius (float): radius
+    """
+    dist_2 = np.diag((nodes - barycenter) @ (nodes - barycenter).T)
+    return np.any(np.array([d < radius**2 for d in dist_2]))
+
+
+@njit
+def clip_to_zero(x, tol: float = 1e-5):
+    """
+    clip_to_zero:
         set values under threshold to zero.
-    
-    @param w (np.array): numpy array of dimension 1, such that sum(w) = 1.
-    @param tol (float): tolerance
+
+    Args:
+        x (np.array): numpy array of dimension 1.
+        tol (float): tolerance.
     """
-    w[w<tol] = 0
-    return w/np.sum(w)
+    x[x < tol] = 0.0
+    return x / np.sum(x)
 
 
 # ------------------------------------------------------------------------------
@@ -125,136 +156,135 @@ def Tzero(w, tol=1e-5):
 # ------------------------------------------------------------------------------
 def incremental_pure_synth(X1, X0):
     """
-    incremental_pure_synth: 
+    incremental_pure_synth:
         main algorithm, find the vertices of the simplex that X1 falls into
         returns the points and the antiranks
-    
-    @param X1 (np.array): array of dimension p of the treated unit
-    @param X0 (np.array): n x p array of untreated units
+
+    Args:
+        X1 (np.array): array of dimension p of the treated unit
+        X0 (np.array): n x p array of untreated units
 
     """
     # get the ranks and anti-ranks of X0 with respect to their distances to X1
-    ranks, antiRanks = get_ranks(X1, X0)
+    _, antiRanks = get_ranks(X1, X0)
     n0, p = X0.shape
-    
+
     # Initialize variables
     foundIt, inHullFlag = False, False
-    
-    ### BIG LOOP ###
+
     # We loop over number of possible points, starting from p+1
-    for k in range(p+1, n0+1):        
+    for k in range(p + 1, n0 + 1):
         # init the_simplex variable if there is a problem
         # when points is not inside convex hull, returns all the points
-        the_simplex =  tuple(range(k))
-        
-        # 1. Set of 'k' nearest neighbors
+        the_simplex = tuple(range(k))
+
+        # 1. Check if X1 belongs to the convex hull defined by its k nearest neighbors
         X_NN = X0[antiRanks[:k],]
-        
-        # 2. Check if X1 belongs to that convex hull
+
         if not inHullFlag:
-            inHullFlag = in_hull(X1, X_NN)
-        
+            inHullFlag = is_in_hull(X1, X_NN)
+
         if not inHullFlag:
-            continue # skip to next iteration if X1 not in convex hull of nearest neighbors
-        
-        # 3. For all the subsets of cardinality p+1 that have x in their convex hull...
+            continue  # skip to next iteration if X1 not in convex hull of nearest neighbors
+
+        # 2. For all the subsets of cardinality p+1 that have x in their convex hull...
         #  (since previous simplices did not contain X1,
         #   we need only to consider the simplices that have the new nearest neighbors as a vertex)
         # ...check if a point in X0 is contained in the circumscribing hypersphere of any of these simplices
-        for item in itertools.combinations(range(k-1),p):
-            candidate = item + (k-1,)
-            if in_hull(X1, X_NN[candidate,]):
+        for item in itertools.combinations(range(k - 1), p):
+            candidate = item + (k - 1,)
+            if is_in_hull(X1, X_NN[candidate,]):
                 try:
-                    radius, center = compute_radius_and_barycenter(X_NN[candidate,]) # sometimes gives an error if points have the same values for a particular X0
+                    radius, center = compute_radius_and_barycenter(
+                        X_NN[candidate,]
+                    )  # sometimes gives an error if points have the same values for a particular X0
                 except:
                     radius = np.nan
-                
-                if np.isnan(radius): # if there is a degenerate case, we stop
+
+                if np.isnan(radius):  # if there is a degenerate case, we stop
                     the_simplex = candidate
                     foundIt = True
                     break
-            
-                if not inside_sphere(np.delete(X0, antiRanks[candidate,], 0), center, radius):
+
+                if not is_inside_sphere(
+                    np.delete(X0, antiRanks[candidate,], 0), center, radius
+                ):
                     the_simplex = candidate
                     foundIt = True
                     break
         if foundIt:
             break
-            
+
     antiRanks_tilde = sorted(antiRanks[the_simplex,])
     return X0[antiRanks_tilde,], antiRanks_tilde
 
 
-def pensynth_weights(X0, X1, pen=0.0, V=None):
+def pensynth_weights(X0, X1, pen: float = 0.0, V=None):
     """
     pensynth_weights:
         computes penalized synthetic control weights with penalty pen
-    
+
     See "A Penalized Synthetic Control Estimator for Disaggregated Data"
-    
-    @param X0 (np.array): n x p matrix of untreated units
-    @param X1 (np.array): 1 x p matrix of the treated unit
-    @param pen (float): lambda, positive tuning parameter
-    @param V (np.array): weights for the norm
+
+    Args:
+        X0 (np.array): n x p matrix of untreated units
+        X1 (np.array): 1 x p matrix of the treated unit
+        pen (float): lambda, positive tuning parameter
+        V (np.array): weights for the norm
     """
     if V is None:
         V = np.identity(X0.shape[1])
     n0 = len(X0)
-    
+
     # OBJECTIVE
-    delta = np.diag((X0-X1) @ V @ np.transpose(X0-X1))
-    P = matrix(X0 @ V @ np.transpose(X0))
-    q = matrix(-X0 @ V @ X1 + (pen/2)*delta)
-    
+    delta = np.diag((X0 - X1) @ V @ (X0 - X1).T)
+    P = matrix(X0 @ V @ X0.T)
+    q = matrix(-X0 @ V @ X1 + pen * delta / 2)
+
     # ADDING-UP TO ONE
-    A = matrix(1.0, (1,n0))
+    A = matrix(1.0, (1, n0))
     b = matrix(1.0)
-    
+
     # NON-NEGATIVITY
     G = matrix(-np.identity(n0))
     h = matrix(np.zeros(n0))
-    
+
     # COMPUTE SOLUTION
-    solvers.options['show_progress'] = False
-    solvers.options['abstol'] = 1e-8
-    solvers.options['reltol'] = 1e-8
-    solvers.options['maxiters'] = 500
     sol = solvers.qp(P, q, G, h, A, b)
-    return Tzero(np.squeeze(np.array(sol['x'])))
+    return clip_to_zero(np.squeeze(np.array(sol["x"])))
 
 
-if __name__=='__main__':    
+if __name__ == "__main__":
     # Test with simulated data
-    n = 11
-    p = 5
+    n = 10
+    p = 6
 
-    X = np.random.normal(0, 1, size=(n, p))
-    X1 = X[0]
-    X0 = np.delete(X, (0), axis=0)
+    X0 = np.random.normal(0, 1, size=(n, p))
+    X1 = X0.mean(axis=0)
 
-    in_hull_flag = in_hull(X1, X0)
-    if in_hull_flag:
+    is_in_hull_flag = is_in_hull(X1, X0)
+    if is_in_hull_flag:
         print("Treated is inside convex hull.")
     else:
         print("Treated not in convex hull.")
-    
-    print("="*80)
+
+    print("=" * 80)
     print("Method 1 : Compute Delaunay Triangulation of X0")
-    print("="*80)
-    
+    print("=" * 80)
+
     start_time = time.time()
     tri = Delaunay(X0)
     any_simplex = tri.find_simplex(X1)
-    print(any_simplex>=0)
-    the_simplex_Delaunay = tri.simplices[any_simplex]
-    print(X0[sorted(the_simplex_Delaunay),])
-    print(f"Temps d'exécution total : {(time.time() - start_time):.7f} secondes ---")
+    print(any_simplex >= 0)
+    delaunay_simplex = tri.simplices[any_simplex]
+    print(X0[sorted(delaunay_simplex),])
+    print(f"Runtime : {(time.time() - start_time):.2f} sec")
 
-    print("="*80)
+    print("=" * 80)
     print("Method 2 : incremental algorithm")
-    print("="*80)
-    
+    print("=" * 80)
+
     start_time = time.time()
     simplex, _ = incremental_pure_synth(X1=X1, X0=X0)
     print(simplex)
-    print(f"Temps d'exécution total : {(time.time() - start_time):.7f} secondes ---")
+    print(f"Runtime : {(time.time() - start_time):.2f} sec")
